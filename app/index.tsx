bbf925068c1e97096b227c8e34837b126273e7e0
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-04-16 23:36:14 +0200

    little bonus, keyboard sticky is perfect for floating buttons making sure it stays above the keyboard

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,10 @@ import { GlassView, isLiquidGlassAvailable } from "expo-glass-effect";
 import { useRouter } from "expo-router";
 import { useState } from "react";
 import { Pressable, RefreshControl, View } from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-controller";
+import {
+  KeyboardAwareScrollView,
+  KeyboardStickyView,
+} from "react-native-keyboard-controller";
 
 export default function Index() {
   const { days, tasksByDate, toggleTask, deleteTask, addTask, refresh } =
@@ -60,29 +63,31 @@ export default function Index() {
         })}
       </KeyboardAwareScrollView>
 
-      <GlassView
-        style={{
-          position: "absolute",
-          bottom: 20,
-          right: 20,
-          width: 40,
-          height: 40,
-          borderRadius: 20,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: isLiquidGlassAvailable()
-            ? "transparent"
-            : theme.placeholderText,
-        }}
-      >
-        <Pressable onPress={() => router.push("/temp-notes")}>
-          <Ionicons
-            name="document-text-outline"
-            size={22}
-            color={theme.titleText}
-          />
-        </Pressable>
-      </GlassView>
+      <KeyboardStickyView>
+        <GlassView
+          style={{
+            position: "absolute",
+            bottom: 20,
+            right: 20,
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: isLiquidGlassAvailable()
+              ? "transparent"
+              : theme.placeholderText,
+          }}
+        >
+          <Pressable onPress={() => router.push("/temp-notes")}>
+            <Ionicons
+              name="document-text-outline"
+              size={22}
+              color={theme.titleText}
+            />
+          </Pressable>
+        </GlassView>
+      </KeyboardStickyView>
     </View>
   );
 }
