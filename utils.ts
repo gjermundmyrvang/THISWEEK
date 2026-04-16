3a53b9ae6cae6e525a20d1adfda252aa82369557
gjermundmyrvang <myrvang.gjermund@gmail.com>
2026-04-16 14:46:44 +0200

    toISOString() converts the date to UTC, that caused the weird results when refreshing after time had past 23:59
    when the clock rolls over to 00:00 locally, the UI re-rendered with the
    new local date, but my function cleanPastDays and all task lookups still
    resolved to the UTC date, which therefore lagged behind... should be
    fixed by this commit

--- utils.ts
+++ utils.ts
@@ -30,4 +30,9 @@ export function formatDate(date: Date) {
   });
 }
 
-export const getKey = (date: Date) => date.toISOString().split("T")[0];
+export const getKey = (date: Date): string => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
